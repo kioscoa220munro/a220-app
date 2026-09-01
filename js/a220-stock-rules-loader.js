9529dce87f63c8b140cb65468b17c74d46a91192
kioscoa220munro <kiosco.a220@gmail.com>
2026-09-01 19:40:51 -0300

    A220: preparar reglas de stock

---
+++ js/a220-stock-rules-loader.js
@@ -0,0 +1,2 @@
+// A220 Pro - carga de reglas de stock
+(function(){const s=document.createElement('script');s.src='a220-stock-rules-v2.js';s.onload=()=>window.a220ApplyStockRules?.();document.head.appendChild(s)})();
