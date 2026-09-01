c31e0fd318f25d86d892cb7daa1d9b6ef3fc7d34
kioscoa220munro <kiosco.a220@gmail.com>
2026-09-01 19:43:06 -0300

    A220: limpiar loader temporal

--- js/a220-stock-rules-loader.js
+++
@@ -1,2 +0,0 @@
-// A220 Pro - carga de reglas de stock
-(function(){const s=document.createElement('script');s.src='a220-stock-rules-v2.js';s.onload=()=>window.a220ApplyStockRules?.();document.head.appendChild(s)})();
